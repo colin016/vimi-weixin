23051a1c640db16689414fb5e303b3a74c348982
stranbird <stranbird@gmail.com>
2013-07-03 15:12:26 +0800

    added entry point

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
+require 'digest/sha1'
 class SessionsController < ApplicationController
   def entry
+  	my_params = {token: 'molitown'}
+  	my_params.merge! params.select { |k, v| k.to_s.match /timestamp|nonce/ }
+  	text = my_params.values.sort.join
+  	hash_text = Digest::SHA1.hexdigest text
+
+  	# render :json => {params: my_params.values.sort, signature: params['signature'], my_signature: hash_text}
+  	render text: params['echostr'] if params['signature'] == hash_text
   end
 end
