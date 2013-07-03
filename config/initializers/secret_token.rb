719b0c6cf904d7ecc29393178f998da05be13320
stranbird <stranbird@gmail.com>
2013-07-03 14:37:14 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PureFortress9858::Application.config.secret_token = 'f0b299c415d2dbf12ba0c442b6eea537002552b1d3bf1e5cd519bf1c0820c8b3375773f1cb8d4f7c53480908fb93a2ee15bf2d73187b150d2f9816f61d1dffc3'
