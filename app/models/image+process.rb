27427caa94854b38c6e74fe81e19696471089abe
stranbird <stranbird@gmail.com>
2013-07-25 17:36:37 +0800

    命名污染啊！！！

--- app/models/image+process.rb
+++ app/models/image+process.rb
@@ -1,29 +1,29 @@
 # encoding: utf-8
 
-require 'RMagick'
 
-include Magick
+# include Magick
 
 module ImageWithProcess
+  require 'RMagick'
 
   def front
-    canvas = Image.new(453, 332) { self.background_color = '#fefefe' }
-    image = ImageList.new("#{Rails.public_path}#{self.path}")
-    canvas.composite!(image.crop(0, 0, 391, 272), 30, 31, AtopCompositeOp)
+    canvas = Magick::Image.new(453, 332) { self.background_color = '#fefefe' }
+    image = Magick::ImageList.new("#{Rails.public_path}#{self.path}")
+    canvas.composite!(image.crop(0, 0, 391, 272), 30, 31, Magick::AtopCompositeOp)
 
     canvas
   end
 
   def back
-    @canvas = ImageList.new("/Users/stranbird/Downloads/postcard无文字.png")
-    draw = Draw.new
+    @canvas = Magick::ImageList.new("/Users/stranbird/Downloads/postcard无文字.png")
+    draw = Magick::Draw.new
     draw.annotate(@canvas, 0, 0, 36, 48, "200240") do
       self.kerning = 10
       self.pointsize = 18
     end
 
     text = "我们很容易被那些纸醉金迷、灯红酒绿，或者类似的我们所从未了解过的世界而吸引，充满好奇和兴趣。但我们也从未对描述这些生活的文字载体产生任何依恋。当我们不再目眩神驰、满纸烟云的时候，那些平淡而缺乏克制的庸俗便会让我们厌倦，最终只是成为我们茶余饭后的消遣。而我们的生活，它依然在脚下。"
-    draw = Draw.new
+    draw = Magick::Draw.new
     
     draw.annotate(@canvas, 0, 0, 40, 84, wrap_text(text, 13)) do 
       self.font = "/Users/stranbird/Library/Fonts/myfont.ttf"
@@ -38,7 +38,7 @@ module ImageWithProcess
   def address(name, loc)
     text = "#{name}(收)\n#{wrap_text(loc, 10)}"
 
-    draw = Draw.new
+    draw = Magick::Draw.new
     draw.annotate(@canvas, 0, 0, 276, 120, text) do
       self.font = "/Users/stranbird/Library/Fonts/myfont.ttf"
       self.pointsize = 15
