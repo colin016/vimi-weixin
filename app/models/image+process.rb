32390a4105d0c380216e9d3adec4b0398350cb05
stranbird <stranbird@gmail.com>
2013-07-26 15:58:38 +0800

    修改了模板和参数。

--- app/models/image+process.rb
+++ app/models/image+process.rb
@@ -2,52 +2,59 @@
 
 
 # include Magick
+# class Rails
+#   def self.public_path
+#     ''
+#   end
+# end
 
 module ImageWithProcess
   require 'RMagick'
  
   def front
-    canvas = Magick::Image.new(453, 332) { self.background_color = '#fefefe' }
-    image = Magick::ImageList.new("#{Rails.public_path}#{self.path}")
+    canvas = Magick::ImageList.new(front_template_path)
+    image = Magick::ImageList.new(path)
     image.format = 'jpg'
     canvas.format = 'png'
-    canvas.composite!(image.resize_to_fill(391, 272), 30, 31, Magick::AtopCompositeOp)
+    canvas.composite!(image.resize_to_fill(815, 562), 65, 65, Magick::AtopCompositeOp)
 
     canvas
   end
 
   def back
-    @canvas = Magick::ImageList.new("#{Rails.public_path}/images/postcard_template.png")
+    @canvas = Magick::ImageList.new(back_template_path)
     draw = Magick::Draw.new
-    draw.annotate(@canvas, 0, 0, 36, 48, self.order.receiver_code || ' ') do
-      self.kerning = 10
-      self.pointsize = 18
+    draw.annotate(@canvas, 0, 0, 73, 100, code || ' ') do
+      self.kerning = 20
+      self.pointsize = 40
     end
 
-    text = self.order.content || "Hello World!"
+    text = content || "Hello World!"
     # text = "我们很容易被那些纸醉金迷、灯红酒绿，或者类似的我们所从未了解过的世界而吸引，充满好奇和兴趣。但我们也从未对描述这些生活的文字载体产生任何依恋。当我们不再目眩神驰、满纸烟云的时候，那些平淡而缺乏克制的庸俗便会让我们厌倦，最终只是成为我们茶余饭后的消遣。而我们的生活，它依然在脚下。"
     draw = Magick::Draw.new
     
-    draw.annotate(@canvas, 0, 0, 40, 84, wrap_text(text, 13)) do 
+    draw.annotate(@canvas, 0, 0, 80, 170, wrap_text(text, 17)) do 
       self.font = "#{Rails.public_path}/fonts/myfont.ttf"
-      self.pointsize = 16
+      # self.font = '/Users/stranbird/dev/vimin-weixin/public/fonts/myfont.ttf'
+      self.pointsize = 25
     end
 
-    address(self.order.receiver_name, self.order.receiver_address)
+    write_address(name, address)
 
     @canvas[0]
   end
 
-  def address(name, loc)
+  def write_address(name, loc)
     name ||= ''
     loc ||= ''
     text = "#{name}(收)\n#{wrap_text(loc, 10)}"
 
     draw = Magick::Draw.new
-    draw.annotate(@canvas, 0, 0, 276, 120, text) do
+    draw.annotate(@canvas, 0, 0, 570, 260, text) do
       self.font = "#{Rails.public_path}/fonts/myfont.ttf"
-      self.pointsize = 15
-      self.interline_spacing = 12
+      # self.font = "/Users/stranbird/dev/vimin-weixin/public/fonts/myfont.ttf"
+      self.pointsize = 25
+      self.interline_spacing = 23
     end
   end
 
@@ -64,4 +71,43 @@ module ImageWithProcess
       path
     end
   end
+
+  # def path
+  #   '/Users/stranbird/dev/cat'
+  # end
+
+  def name
+    self.order.receiver_name
+    # "陈乐乐"
+  end
+
+  def address
+    self.order.receiver_address
+    # "上海市闵行区东川路 800号"
+  end
+
+  def content
+    self.order.content
+    # "200240"
+  end
+
+  def code
+    self.order.receiver_code
+    # "200240"
+  end
+
+  def back_template_path
+    "#{Rails.public_path}/images/postcard-02.png"
+    # "/Users/stranbird/Downloads/postcard-02.png"
+  end
+
+  def front_template_path
+    "#{Rails.public_path}/images/postcard-01.png"
+    # "/Users/stranbird/Downloads/postcard-01.png"
+  end
 end
+
+# include ImageWithProcess
+
+# preview_paths.first { |e| `open #{e}`}
+# `open #{preview_paths.first}`
